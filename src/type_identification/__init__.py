f5c497b7125fdb14ae5afaa088d369c98fbc4d11
pyran <121752109+pyran19@users.noreply.github.com>
2026-03-20 04:37:19 +0900

    Add typeless defensive candidate

--- src/type_identification/__init__.py
+++ src/type_identification/__init__.py
@@ -1,6 +1,9 @@
 """Utilities for identifying single Pokémon defending types from damage multipliers."""
 
 from .solver import (
+    ATTACK_TYPE_NAMES,
+    DEFENDING_TYPE_NAMES,
+    TYPELESS_DEFENDING_TYPE,
     TYPE_NAMES,
     build_signature_map,
     find_minimum_attack_type_sets,
@@ -8,6 +11,9 @@ from .solver import (
 )
 
 __all__ = [
+    "ATTACK_TYPE_NAMES",
+    "DEFENDING_TYPE_NAMES",
+    "TYPELESS_DEFENDING_TYPE",
     "TYPE_NAMES",
     "build_signature_map",
     "find_minimum_attack_type_sets",
